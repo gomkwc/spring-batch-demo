4fd01f347b22013618ebc2f8a29a227b4e50274e
gomkwc <gomkwc@gmail.com>
2014-09-15 13:46:28 +0900

    first

---
+++ src/main/java/com/mkyong/model/ReportRowMapper.java
@@ -0,0 +1,27 @@
+package com.mkyong.model;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * User: David KIM
+ * Date: 14. 9. 15.
+ * Time: 오후 1:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReportRowMapper  implements RowMapper<Report> {
+
+    @Override
+    public Report mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Report report = new Report();
+        report.setDate(resultSet.getString("date"));
+        report.setImpressions(resultSet.getString("impressions"));
+        report.setClicks(resultSet.getString("clicks"));
+        report.setEarning(resultSet.getString("earning"));
+        return report;
+    }
+
+}
+
